a2ae7d06f5fe2965e0fd1057832f0d0c138fda46
thylux <thylux@gmail.com>
2016-04-14 10:22:10 +0100

    Basic server with direct xlsx read

--- test_server/server.js
+++ test_server/server.js
@@ -4,46 +4,30 @@ var app = express();
 if(typeof require !== 'undefined') XLSX = require('xlsx');
 var workbook = XLSX.readFile('data/skills.xls');
 
+function to_json(worksheet) {
+	return XLSX.utils.sheet_to_row_object_array(worksheet);
+}
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
 app.get('/groups', function (req, res) {
-    var first_sheet_name = workbook.SheetNames[0];
-    var address_of_cell = 'A1';
-    
-    /* Get worksheet */
-    var worksheet = workbook.Sheets[first_sheet_name];
-    
-    /* Find desired cell */
-    var desired_cell = worksheet[address_of_cell];
-    
-    /* Get the value */
-    var desired_value = desired_cell.v;
+    var worksheet = workbook.Sheets[workbook.SheetNames[1]];
+
+    res.send(to_json(worksheet));
 });
 
 app.get('/domains', function (req, res) {
-    var first_sheet_name = workbook.SheetNames[1];
-    var address_of_cell = 'A1';
-    
-    /* Get worksheet */
-    var worksheet = workbook.Sheets[first_sheet_name];
-    
-    /* Find desired cell */
-    var desired_cell = worksheet[address_of_cell];
-    
-    /* Get the value */
-    var desired_value = desired_cell.v;
+    var worksheet = workbook.Sheets[workbook.SheetNames[2]];
+
+    res.send(to_json(worksheet));
 });
 
 app.get('/skills', function (req, res) {
-    var first_sheet_name = workbook.SheetNames[2];
-    var address_of_cell = 'A1';
-    
-    /* Get worksheet */
-    var worksheet = workbook.Sheets[first_sheet_name];
-    
-    /* Find desired cell */
-    var desired_cell = worksheet[address_of_cell];
-    
-    /* Get the value */
-    var desired_value = desired_cell.v;
+    var worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+    res.send(to_json(worksheet));
 })
 
 var server = app.listen(8081, function () {
